a5a81235d0103d6a8531f4e1de8de1ebcc29050c
MuhammedEmin <08muhammedeminbal@gmail.com>
2021-09-13 10:16:52 +0300

    tum gorevler eklendi

--- project.py
+++ project.py
@@ -109,3 +109,95 @@ df.groupby("SOURCE").agg({"PRICE": "mean"})
 ##################################
 df.groupby(["SOURCE", "COUNTRY"]).agg({"PRICE": "mean"})
 
+############################
+# ----- GÖREV 2 -----
+
+# COUNTRY, SOURCE, SEX, AGE kırılımında ortalama kazançlar nedir?
+###########################
+group_df = grupla(dataframe=df, colname=["COUNTRY", "SOURCE", "SEX", "AGE"], agg_colname="PRICE", agg="mean", turn=True)
+# or
+group_df = df.groupby(["COUNTRY", "SOURCE", "SEX", "AGE"]).agg({"PRICE": "mean"})
+
+############################
+# ----- GÖREV 3 -----
+
+# Çıktıyı PRICE’a göre sıralayınız.
+###########################
+agg_df = group_df.sort_values("PRICE", ascending=False)
+
+############################
+# ----- GÖREV 4 -----
+
+# Index’te yer alan isimleri değişken ismine çeviriniz.
+###########################
+agg_df = agg_df.reset_index()
+
+############################
+# ----- GÖREV 5 -----
+
+# age değişkenini kategorik değişkene çeviriniz ve agg_df’e ekleyiniz
+###########################
+# example : https://pbpython.com/pandas-qcut-cut.html
+bins = [0, 18, 23, 30, 40, 70]
+labels = ["0_18", "19_23", "24_30", "31_40", "41_70"]
+
+
+def cutt(dataframe, colname, bins, labels=None):
+    if labels != None:
+        cut_series = pd.cut(dataframe[colname],
+                            labels=labels,
+                            bins=bins)
+    else:
+        cut_series = pd.cut(dataframe[colname],
+                            bins=bins)
+    return cut_series
+
+
+agg_df["AGE_CAT"] = cutt(agg_df, "AGE", bins, labels)
+
+############################
+# ----- GÖREV 6 -----
+
+# Yeni seviye tabanlı müşterileri (persona) tanımlayınız
+###########################
+# collect the string and assign to variable customer_level_based
+agg_df["customer_level_based"] = [col[0].upper() + "_" + col[1].upper() + "_" + col[2].upper() + "_" + col[5] for col in
+                        agg_df.values]
+
+agg_df_new = agg_df.groupby("customer_level_based").agg({"PRICE": "mean"})
+agg_df_new = agg_df_new.reset_index()
+
+############################
+# ----- GÖREV 7 -----
+
+# Yeni müşterileri (personaları) segmentlere ayırınız
+###########################
+
+agg_df_new["SEGMENT"] = pd.qcut(agg_df_new["PRICE"], q=4, labels=["D", "C", "B", "A"])
+
+# analysis
+
+agg_df_new.groupby("SEGMENT").agg({"PRICE": ["mean", "min", "max", "sum"]})
+segment_c = agg_df_new.loc[agg_df_new["SEGMENT"] == "C", :]
+check_df(segment_c)
+
+segment_c["customer_level_based"].unique()
+segment_c["customer_level_based"].nunique()
+
+agg_df_new.describe().T
+
+############################
+# ----- GÖREV 8 -----
+
+# Yeni gelen müşterileri segmentlerine göre sınıflandırınız ve ne kadar gelir getirebileceğini tahmin ediniz
+###########################
+# PREDICT
+
+new_user = "TUR_ANDROID_FEMALE_31_40"
+new_user2 = 'FRA_IOS_FEMALE_31_40'
+
+predict_user_1 = agg_df_new[agg_df_new["customer_level_based"] == new_user]
+predict_user_2 = agg_df_new[agg_df_new["customer_level_based"] == new_user2]
+
+print(predict_user_1)
+print(predict_user_2)
